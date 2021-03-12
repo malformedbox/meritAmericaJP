6b92c22dd3ad34f16ac82cbf656dfc11df168b27
patchworkjp@gmail.com <jenny.prany@gmail.com>
2021-03-12 13:09:30 -0600

    pushed to view current status on github

---
+++ src/main/java/com/meritamerica/assignment3/CheckingAccount.java
@@ -0,0 +1,13 @@
+package com.meritamerica.assignment3;
+
+import java.text.*;
+
+public class CheckingAccount extends BankAccount {
+	public CheckingAccount() {} //Default constructor
+	//Create appropriate constructors
+	
+	static CheckingAccount readFromString(String accountData) throws ParseException{
+		//Should throw a java.lang.NumberFormatException if String cannot be correctly parsed
+		return null;
+	}
+}
---
+++ src/main/java/com/meritamerica/assignment3/SavingsAccount.java
@@ -0,0 +1,12 @@
+package com.meritamerica.assignment3;
+
+import java.text.*;
+
+public class SavingsAccount extends BankAccount{
+	public SavingsAccount() {} //Default constructor
+	
+	static SavingsAccount readFromString(String accountData) throws ParseException{
+		//Should throw a java.lang.NumberFormatException if String cannot be correctly parsed
+		return null;
+	}
+}
