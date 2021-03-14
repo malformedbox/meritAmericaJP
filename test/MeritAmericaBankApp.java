1aafc2a5b3800cd645d83311d913a4f4e58a59bd
patchworkjp@gmail.com <jenny.prany@gmail.com>
2021-03-14 03:05:38 -0500

    removed test files

--- test/MeritAmericaBankApp.java
+++
@@ -1,9 +0,0 @@
-package com.meritamerica.assignment3;
-
-public class MeritAmericaBankApp {
-	public static void main(String[] args) throws Exception {
-		MeritBank.readFromFile("src/test/testMeritBank_good.txt");
-		
-		//MeritBank.printAccountList();
-	}
-}
