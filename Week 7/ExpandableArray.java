e870adfaa749b1bccc2f00f3246c07c9073d3c46
patchworkjp@gmail.com <jenny.prany@gmail.com>
2021-03-24 14:04:41 -0500

    added in comments of ExpandableArray, question 1

--- Week 7/ExpandableArray.java
+++ Week 7/ExpandableArray.java
@@ -2,6 +2,13 @@ package week6;
 
 import acm.program.ConsoleProgram;
 
+/* 1. Primitive vs. Objects
+	String name = readLine("Enter name: ");
+	Name is the type String. String is an object, to compare Strings, you would need to compare references (use name.equals)
+	(name == "Q") would not work because == is used for primitive types (int, double, boolean, char)
+	- In the second example, (ch == 'Q') will work because ch is a char, which is a primitive type.
+ */
+
 public class ExpandableArray extends ConsoleProgram {
 	private Object[] array;
 	
